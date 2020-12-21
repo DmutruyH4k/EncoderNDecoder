be43454c92683733b57fdb4bee0e398a1764c884
DmutruyH4k <38806526+DmutruyH4k@users.noreply.github.com>
2020-12-21 09:17:18 +0300

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -1,4 +1,6 @@
 from random import choice, randint
+from spellchecker import SpellChecker
+spell = SpellChecker()
 
 
 def encode(txt):
@@ -28,9 +30,18 @@ def encode(txt):
 
 	splt = randint(1, 6)
 	while splt < len(add_txt):
+		if spell.correction(add_txt[0:splt]) == add_txt[0:splt]:
+			continue
 		txt.insert(randint(1, len(txt)), add_txt[0:splt])
 		add_txt = add_txt.replace(add_txt[0:splt], '')
 		splt = randint(1, 6)
 
 	txt.insert(randint(1, len(txt)), add_txt[0:splt])
 	return ' '.join(txt)
+
+
+if __name__ == '__main__':
+	txt_to_encode = input("Input text: ").lower()
+	encoded = encode(txt_to_encode)
+	print(encoded)
+	input('Press Enter to continue...')
